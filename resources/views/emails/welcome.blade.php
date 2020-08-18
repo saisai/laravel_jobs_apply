f562fb7b3702e3d49a06a92f8e85b48b95bdfdfa
saisai <you@example.com>
2020-08-19 06:34:27 +0700

    init

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -19,43 +19,8 @@
 </head>
 <body>
 	<div class="myheader">
-	<h2>Nyein Pe</h2>
-	<address>
-	7/46, Flat Bonkai, Building Kor.1,<br />
-	3rd Flr, Bonkai, Rama 4 Rd., Lumpini, <br />
-	Pathum Wan, Bangkok
-	</address>
-	<p>083 6969 423</p>
-	<p>hello@gmail.com</p>
-	</div>
-	<br />
-	<hr />
-	<br />
-	<div class="mydate">
-	<p><?php echo date("F d, Y") ?></p>
-	</div>
-	<br />
-	<div class="mybody">
-		<p>Dear Sir/Madam,</p>
-		<br />
-		<p>I am writing to apply for the programmer position advertised.<br />
-		As requested, I enclose a job application.
-		</p>
-		<br />
-		<p>I have been working in Walen Education Co. Ltd since 2013 as a Web developer.<br/ >
-		I have a wide and practical knowledge of technologies: PHP, Python, Mysql, Javascript, HTML and CSS.<br/ >
-		I can manage web hostings like Amazon Web Services (AWS) and Vultr Global Cloud Hosting.
-		</p>
-		<br/ >
-		
-		
-		<p>Thank you for your time and consideration.<br />
-		I look forward to hearing from you soon.
-		</p>
-		
-		<br />
-		<p>Sincerely,</p>
-		<p>Nyein Pe	</p>
+	<h2>Hello world</h2>
+
 	</div>
 	
 </body>
